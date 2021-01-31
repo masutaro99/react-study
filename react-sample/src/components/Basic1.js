cebb8d04a09bd543993d55a43cc0dbac39b5f234
masutaro99 <maskenpa1001@gmail.com>
2021-01-31 23:09:46 +0900

    add: add button and introduce useState

--- react-sample/src/components/Basic1.js
+++ react-sample/src/components/Basic1.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Basic1 = (props) => {
   const clickHandler = () => {
     console.log('clicked')
   }
 
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      <button onClick={clickHandler}>Click</button>
+      <button onClick={()=>{setCount(count+1)}}>Click{count}</button>
+      
       <h1>Hello {props.name}</h1>
     </div>
   )
