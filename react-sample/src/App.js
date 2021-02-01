a9faef117520b3a5e838b5f0594f47d91be99132
masutaro99 <maskenpa1001@gmail.com>
2021-02-01 17:22:44 +0900

    add: add timer using useState

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Basic1 from "./components/Basic1"
+// import Basic1 from "./components/Basic1"
 import Basic2 from "./components/Basic2"
 import BasicUseEffect from "./components/BasicUseEffect"
+import TimerContainer from "./components/TimerContainer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1/>
+        {/* <Basic1/> */}
         <Basic2/>
         <BasicUseEffect/>
+        <TimerContainer/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
